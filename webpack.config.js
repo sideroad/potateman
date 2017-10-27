11e7939a5df5dfa2d1fc76326218fb92c22b8bbd
sideroad <sideroad.jp@gmail.com>
2017-10-28 08:55:40 +0900

    Cache handle improvement

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const WebpackOnBuildPlugin = require('on-build-webpack');
 
 module.exports = {
-  entry: './src/client.js',
+  entry: {
+    main: ['./src/client.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'dist/static'),
-    filename: 'bundle.js'
+    filename: '[name]-[chunkhash].js',
+    chunkFilename: '[name]-[chunkhash].js',
   },
   module: {
     loaders: [
@@ -13,9 +17,23 @@ module.exports = {
         loaders: 'buble-loader',
         include: path.join(__dirname, 'src'),
         query: {
-          objectAssign: 'Object.assign'
-        }
-      }
-    ]
-  }
+          objectAssign: 'Object.assign',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackOnBuildPlugin((stats) => {
+      // eslint-disable-next-line
+      const fs = require('fs');
+      const bundleName = Object.keys(stats.compilation.assets)[0];
+      const htmlPath = path.resolve(__dirname, 'dist/static/index.html');
+      const html = fs.readFileSync(htmlPath, 'utf8');
+      // eslint-disable-next-line prefer-template
+      fs.writeFileSync(htmlPath, html.replace('/bundle.js', '/' + bundleName), 'utf8');
+    }),
+  ],
+  externals: {
+    fs: {},
+  },
 };
