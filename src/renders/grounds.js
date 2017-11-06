fd47fcbc25373b48056147cd0e22457262a11237
sideroad <sideroad.jp@gmail.com>
2017-11-07 07:53:31 +0900

    Bugfix of ground size

--- src/renders/grounds.js
+++ src/renders/grounds.js
@@ -34,33 +34,32 @@ export default function ({ engine, size, stage = 'hexagon' }) {
       mask: COLLISION.POTATEMANS,
     },
     render: {
-      fillStyle: 'transparent',
+      // fillStyle: 'transparent',
       lineWidth: 0,
     },
   };
 
   const make = (x, y, amount) => {
     const groundWidth = amount * cellSize;
-    const xx = x - (width / (groundWidth * 2)) - adjust;
-    const yy = y - adjust;
-    const xxx = (x - (width / (groundWidth * 2))) + ((amount * adjust) - adjust);
+    const xx = x - (groundWidth / 2);
+    const yy = y;
     return [
-      Composites.stack(xx, yy, amount, 1, 0, 0, (_x, _y) =>
+      Composites.stack(xx, yy - adjust, amount, 1, 0, 0, (_x, _y) =>
         Bodies.rectangle(_x, _y, cellSize, cellSize, spriteOptions)),
-      Bodies.rectangle(xxx, yy + adjust, amount * cellSize, cellSize, options),
+      Bodies.rectangle(x, y, groundWidth, cellSize, options),
     ];
   };
-
+  console.log(width, Math.ceil((width / 3) / cellSize));
   const grounds = {
     pentagon: () =>
       [
-        ...make(width / 3, (height / 5) * 4, Math.ceil(width / 2 / cellSize)),
+        ...make(width / 2, (height / 5) * 4, Math.ceil(width / 1.5 / cellSize)),
         ...make(width / 6, height / 2, 6),
         ...make((width / 6) * 5, height / 2, 6),
       ],
     hexagon: () =>
       [
-        ...make(width / 3, height / 2, Math.ceil(width / 3 / cellSize)),
+        ...make(width / 2, height / 2, Math.ceil(width / 2 / cellSize)),
         ...make(width / 6, height / 4, 6),
         ...make(width / 6, (height / 4) * 3, 6),
         ...make((width / 6) * 5, height / 4, 6),
