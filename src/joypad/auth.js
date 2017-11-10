e3d400cda6769abac44a5542609047c3704df612
sideroad <sideroad.jp@gmail.com>
2017-11-10 23:56:02 +0900

    Wait callback until first load.

--- src/joypad/auth.js
+++ src/joypad/auth.js
@@ -5,7 +5,12 @@ export default function authFn(callback) {
     .then((res) => {
       res.json().then((user) => {
         if (user.name) {
-          callback(user);
+          // eslint-disable-next-line no-new
+          const image = new Image();
+          image.onload = () => {
+            callback(user);
+          };
+          image.src = user.image;
         } else {
           document.cookie = `redirect=${window.location.href}; path=/;`;
           window.location.href = '/auth/facebook';
