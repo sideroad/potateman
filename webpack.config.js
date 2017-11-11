79d954a96dd0189e0c357e18c904fb3daf68a399
sideroad <sideroad.jp@gmail.com>
2017-11-11 12:14:06 +0900

    Prevent cache of css

--- webpack.config.js
+++ webpack.config.js
@@ -29,20 +29,32 @@ module.exports = {
       // eslint-disable-next-line
       const fs = require('fs');
       const assetNames = Object.keys(stats.compilation.assets);
+      const version = new Date().getTime();
+      const cssPath = path.resolve(__dirname, 'dist/static/css/base.css');
+      const css = fs.readFileSync(cssPath, 'utf8');
+      // eslint-disable-next-line prefer-template
+      const newCssPath = path.resolve(__dirname, 'dist/static/css/base-' + version + '.css');
+      fs.writeFileSync(newCssPath, css, 'utf8');
 
       // replace to bundle
       const bundleName = assetNames.find(name => /^main-/.test(name));
       const htmlPath = path.resolve(__dirname, 'dist/static/index.html');
       const html = fs.readFileSync(htmlPath, 'utf8');
-      // eslint-disable-next-line prefer-template
-      fs.writeFileSync(htmlPath, html.replace('/bundle.js', '/' + bundleName), 'utf8');
+      fs.writeFileSync(htmlPath, html
+        // eslint-disable-next-line prefer-template
+        .replace('/bundle.js', '/' + bundleName)
+        // eslint-disable-next-line prefer-template
+        .replace('/base.css', '/base-' + version + '.css'), 'utf8');
 
       // replace to mirror
       const mirrorName = assetNames.find(name => /^mirror-/.test(name));
       const mirrorHtmlPath = path.resolve(__dirname, 'dist/static/mirror/index.html');
       const mirrorHtml = fs.readFileSync(mirrorHtmlPath, 'utf8');
-      // eslint-disable-next-line prefer-template
-      fs.writeFileSync(mirrorHtmlPath, mirrorHtml.replace('/bundle-mirror.js', '/' + mirrorName), 'utf8');
+      fs.writeFileSync(mirrorHtmlPath, mirrorHtml
+        // eslint-disable-next-line prefer-template
+        .replace('/bundle-mirror.js', '/' + mirrorName)
+        // eslint-disable-next-line prefer-template
+        .replace('/base.css', '/base-' + version + '.css'), 'utf8');
 
       // replace to joypad
       const joypadName = assetNames.find(name => /^joypad-/.test(name));
@@ -50,7 +62,6 @@ module.exports = {
       const joypadHtml = fs.readFileSync(joypadHtmlPath, 'utf8');
       const joypadCssPath = path.resolve(__dirname, 'dist/static/joypad/joypad.css');
       const joypadCss = fs.readFileSync(joypadCssPath, 'utf8');
-      const version = new Date().getTime();
       // eslint-disable-next-line prefer-template
       const joypadNewCssPath = path.resolve(__dirname, 'dist/static/joypad/joypad-' + version + '.css');
       fs.writeFileSync(joypadNewCssPath, joypadCss, 'utf8');
