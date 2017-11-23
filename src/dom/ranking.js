456e87aabf60052f5a0e0d6a13e5bc7a90dd31a7
sideroad <sideroad.jp@gmail.com>
2017-11-23 22:28:27 +0900

    Fit score bar size by numbers.

--- src/dom/ranking.js
+++ src/dom/ranking.js
@@ -7,7 +7,7 @@ export default function rankingFn({ items }) {
         <td><div class="ranker-ranking ranker-ranking-${index + 1}" >${index + 1}</div></td>
         <td><img class="ranker-image" src="${item.image || '/images/cpu-1.png'}" /></td>
         <td>
-          <div class="ranker-score" style="width: ${(item.score / maxScore) * 150}px;">
+          <div class="ranker-score" style="min-width: ${(item.score / maxScore) * 150}px;">
             <span class="ranker-score-inline">${item.score.toLocaleString()}</span>
           </div>
         </td>
