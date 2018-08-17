74013abd6031046b35ccef38ee3e5638d5f17ade
sideroad <sideroad.jp@gmail.com>
2018-08-17 20:19:38 +0900

    Add dist execution for manifest

--- dist.sh
+++ dist.sh
@@ -5,6 +5,7 @@ mkdir dist
 mkdir dist/static
 cp -Rf src/images dist/static/.
 cp src/index.html dist/static/.
+cp src/manifest.json dist/static/.
 cp -Rf src/joypad dist/static/.
 cp -Rf src/mirror dist/static/.
 cp -Rf src/css dist/static/.
